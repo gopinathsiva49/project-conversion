64402879e9681ea6f0ed39517dc1c37f85e63270
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-05-26 17:20:08 +0530

    added the seed file for test purpose

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(name: "Test User")
+
+Project.create!(name: "Test Project")
