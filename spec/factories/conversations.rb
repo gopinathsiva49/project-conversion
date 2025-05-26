ce1943bb954c98882c3df7e8fe7844e1d471b763
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-05-26 18:16:51 +0530

    removed the unwanted

--- spec/factories/conversations.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :conversation do
-    project { nil }
-    user { nil }
-    entry_type { "" }
-    message { "MyText" }
-  end
-end
--- spec/factories/projects.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :project do
-    name { "MyString" }
-    status { 1 }
-  end
-end
