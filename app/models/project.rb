bbd03b8350b235bf081e8d66648217d2ac0b491e
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-05-26 17:17:50 +0530

    Added validations and associations for the models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+    enum current_status: { open: 0, in_progress: 1, closed: 2, on_hold: 3 }
+    has_many :conversations, dependent: :destroy
+    validates :name, presence: true
+  
 end
