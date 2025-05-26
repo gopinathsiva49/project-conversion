5a7de255dd8c011566fc17ad3f5575111ed9569f
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-05-26 18:07:15 +0530

    model test case is added

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "belongs to a project" do
+    assoc = Conversation.reflect_on_association(:project)
+    expect(assoc.macro).to eq(:belongs_to)
+  end
+
+  it "belongs to a user" do
+    assoc = Conversation.reflect_on_association(:user)
+    expect(assoc.macro).to eq(:belongs_to)
+  end
+
+  it "validates presence of message" do
+    conversation = Conversation.new(message: nil)
+    conversation.validate
+    expect(conversation.errors[:message]).to include("can't be blank")
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates presence of name" do
+    project = Project.new(name: nil)
+    project.validate
+    expect(project.errors[:name]).to include("can't be blank")
+  end
+
+  it "has many conversations" do
+    assoc = Project.reflect_on_association(:conversations)
+    expect(assoc.macro).to eq(:has_many)
+  end
 end
